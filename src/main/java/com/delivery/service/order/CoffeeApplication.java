f60f3f1fbc5f03ca22be4c59b7f74a3f33de55b5
purekite <soocy0718@gmail.com>
2024-05-01 00:41:25 +0900

    :green_heart: CI 수정

--- src/main/java/com/delivery/service/order/CoffeeApplication.java
+++ src/main/java/com/delivery/service/order/CoffeeApplication.java
@@ -1,4 +1,4 @@
-package com.delivery.service.coffee;
+package com.delivery.service.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
